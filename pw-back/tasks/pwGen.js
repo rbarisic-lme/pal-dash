af11d53570b4d2203e03e6183448df9a67b437a6
R. Barisic <xrlabs@gmail.com>
2024-02-23 01:55:44 +0100

    chore: try to fix pwgen

--- pw-back/tasks/pwGen.js
+++ pw-back/tasks/pwGen.js
@@ -3,9 +3,9 @@ import bcrypt from 'bcryptjs';
 
 const args = process.argv.slice(2);
 
-const hashedPassword = await bcrypt.hash(args[0], 10);
+bcrypt.hash(args[0], 10).then(hashedPassword => {
+  const hashedPasswordShellSafe = hashedPassword.replace(/\$/g, '\\$');
 
-const hashedPasswordShellSafe = hashedPassword.replace(/\$/g, '\\$');
-
-console.log('Your hashed password:');
-console.log(hashedPasswordShellSafe);
+  console.log('Your hashed password:');
+  console.log(hashedPasswordShellSafe);
+})
